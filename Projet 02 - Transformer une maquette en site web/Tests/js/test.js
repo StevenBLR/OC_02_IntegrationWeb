2dfedccddbd5a6b6afb9c6bca1178050f38a1c8d
@Sbelair <steven.belair@on-x.com>
2021-03-01 19:28:43 +0100

    First commit

---
+++ Projet 02 - Transformer une maquette en site web/Tests/js/test.js
@@ -0,0 +1,116 @@
+// Liste d'hebergements
+var homes = [
+    {
+        nom : "Auberge la Cannebière",
+        ville : "Marseille",
+        imgUrl : "",
+        prix : 25,
+        note : 4
+    },
+    {
+        nom : "Hôtel du port",
+        ville : "Marseille",
+        imgUrl : "",
+        prix : 25,
+        note : 5
+    },
+    {
+        nom : "Hôtel Les mouettes",
+        ville : "Marseille",
+        imgUrl : "",
+        prix : 76,
+        note : 4
+    },
+    {
+        nom : "Hôtel de la mer",
+        ville : "Marseille",
+        imgUrl : "",
+        prix : 46,
+        note : 3
+    },
+    {
+        nom : "Auberge le Panier",
+        ville : "Marseille",
+        imgUrl : "",
+        prix : 23,
+        note : 4
+    },
+    {
+        nom : "Hotel chez Amina",
+        ville : "Marseille",
+        imgUrl : "",
+        prix : 96,
+        note : 5
+    },
+    {
+        nom : "Hotel Le solei du matin",
+        ville : "Marseille",
+        imgUrl : "",
+        prix : 128,
+        note : 5
+    },
+    {
+        nom : "Au coeur de l'eau Chambres d'hôtes",
+        ville : "Marseille",
+        imgUrl : "",
+        prix : 71,
+        note : 4
+    },
+    {
+        nom : "Hotel Tout bleu et Blanc",
+        ville : "Marseille",
+        imgUrl : "",
+        prix : 68,
+        note : 4
+    }
+];
+// Liste d'activités
+var activities = [
+    {
+        imgUrl : "",
+        ville : "Marseille",
+        nom : "Vieux Port"
+        
+    },
+    {
+        imgUrl : "",
+        ville : "Marseille",
+        nom : "Fort de Pomègues"
+    },
+    {
+        imgUrl : "",
+        ville : "Marseille",
+        nom : "Îles du Frioul"
+    },
+    {
+        imgUrl : "",
+        ville : "Marseille",
+        nom : "Parc National des Calanques",
+    },
+    {
+        imgUrl : "",
+        ville : "Marseille",
+        nom : "Notre-Dame-de-la-Garde"
+    },
+    {
+        imgUrl : "",
+        ville : "Marseille",
+        nom : "Parc Longchamp"
+    }
+];
+
+// Injection des hotels
+function populateHomes(){
+
+}
+
+// Injection des hotels les plus populaires
+function populateMostPopular(){
+
+}
+
+// Injection des activités
+function populateActivities(){
+
+}
+
